277e1cbc80cc0e1b31c7717cf90f1a750d749629
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2021-06-28 15:57:06 -0500

    wtf npi

--- app/controllers/entre_azul_y_verde/registro_controller.rb
+++ app/controllers/entre_azul_y_verde/registro_controller.rb
@@ -1,6 +1,6 @@
 class EntreAzulYVerde::RegistroController < ApplicationController
 	layout 'entre_azul_y_verde'
-	before_action :set_usuario, only: %i[ show edit update destroy ]
+	before_action :set_registro, only: %i[ show edit update destroy ]
 	
 	# GET /usuarios or /usuarios.json
 	def index
@@ -12,7 +12,7 @@ class EntreAzulYVerde::RegistroController < ApplicationController
 	
 	# GET /usuarios/new
 	def new
-		@usuario = Usuario.new
+		@registro = EntreAzulYVerde::EayvUsuario.new
 	end
 	
 	# GET /usuarios/1/edit
@@ -21,15 +21,15 @@ class EntreAzulYVerde::RegistroController < ApplicationController
 	
 	# POST /usuarios or /usuarios.json
 	def create
-		@usuario = Usuario.new(usuario_params)
+		@registro = EntreAzulYVerde::EayvUsuario.new(registro_params)
 		
 		respond_to do |format|
-			if @usuario.save
-				format.html { redirect_to @usuario, notice: "Usuario was successfully created." }
-				format.json { render :show, status: :created, location: @usuario }
+			if @registro.save
+				format.html { redirect_to @registro, notice: "Usuario was successfully created." }
+				format.json { render :show, status: :created, location: @registro }
 			else
 				format.html { render :new, status: :unprocessable_entity }
-				format.json { render json: @usuario.errors, status: :unprocessable_entity }
+				format.json { render json: @registro.errors, status: :unprocessable_entity }
 			end
 		end
 	end
@@ -37,19 +37,19 @@ class EntreAzulYVerde::RegistroController < ApplicationController
 	# PATCH/PUT /usuarios/1 or /usuarios/1.json
 	def update
 		respond_to do |format|
-			if @usuario.update(usuario_params)
-				format.html { redirect_to @usuario, notice: "Usuario was successfully updated." }
-				format.json { render :show, status: :ok, location: @usuario }
+			if @registro.update(usuario_params)
+				format.html { redirect_to @registro, notice: "Usuario was successfully updated." }
+				format.json { render :show, status: :ok, location: @registro }
 			else
 				format.html { render :edit, status: :unprocessable_entity }
-				format.json { render json: @usuario.errors, status: :unprocessable_entity }
+				format.json { render json: @registro.errors, status: :unprocessable_entity }
 			end
 		end
 	end
 	
 	# DELETE /usuarios/1 or /usuarios/1.json
 	def destroy
-		@usuario.destroy
+		@registro.destroy
 		respond_to do |format|
 			format.html { redirect_to usuarios_url, notice: "Usuario was successfully destroyed." }
 			format.json { head :no_content }
@@ -58,12 +58,12 @@ class EntreAzulYVerde::RegistroController < ApplicationController
 	
 	private
 	# Use callbacks to share common setup or constraints between actions.
-	def set_usuario
-		@usuario = Usuario.find(params[:id])
+	def set_registro
+		@registro = EntreAzulYVerde::EayvUsuario.find(params[:id])
 	end
 	
 	# Only allow a list of trusted parameters through.
-	def usuario_params
+	def registro_params
 		params.require(:usuario).permit(:email, :nombre, :apellido_paterno, :apellido_materno, :fecha_nacimiento_nac, :lugar_nacimiento, :curp, :password, :password_confirmation)
 	end
 end
