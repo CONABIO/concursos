bc08dbe6d63ff8ea95a2992d34a1024f417b4141
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2021-10-15 06:56:05 -0500

    escribiendo las correctas asociaciones en direccion, usuario y categoria de mientras

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,9 +1,13 @@
 class Usuario < ApplicationRecord
  
-	has_secure_password
-	has_one :direccion
-	has_many :media
-	has_many :usuario_metadatos
+	#has_secure_password
+	has_one :direccion, inverse_of: :usuario
+	accepts_nested_attributes_for :direccion, allow_destroy: true
 	
+	has_many :medias, inverse_of: :usuario, foreign_key: :usuario_id
+	accepts_nested_attributes_for :medias, allow_destroy: true
+	
+	has_many :usuario_metadatos , inverse_of: :usuario
+	accepts_nested_attributes_for :usuario_metadatos, allow_destroy: true
 	
 end
