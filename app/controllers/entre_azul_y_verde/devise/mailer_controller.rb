1efa2b450b82e202d320394cf0d8ef8af456f456
Carlos Alonso <calonsogeek@gmail.com>
2021-11-04 03:15:14 -0600

    devise acotado solo a entre a zul y verde

---
+++ app/controllers/entre_azul_y_verde/devise/mailer_controller.rb
@@ -0,0 +1,3 @@
+class EntreAzulYVerde::Devise::Mailer < ::Devise::Mailer
+    layout 'entre_azul_y_verde'
+end
---
+++ app/controllers/entre_azul_y_verde/devise/registrations_controller.rb
@@ -0,0 +1,3 @@
+class EntreAzulYVerde::Devise::RegistrationsController < ::Devise::RegistrationsController
+  layout 'entre_azul_y_verde'
+end
