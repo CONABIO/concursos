8fef68b238186ee99570ee08c28320134a3f7c92
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2021-11-16 11:06:54 -0600

    cambiando texto de paisajes

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -8,7 +8,7 @@ class Categoria < ApplicationRecord
 	                 fauna: "Incluye animales nativos de México en vida silvestre; desde pequeños insectos hasta grandes mamíferos, siempre que éstos, no estén en cautiverio o sean animales domésticos y/o mascotas.",
 	                 plantas_y_hongos: "Incluye toda clase de especies vegetales y hongos nativos de México, en vida silvestre. No se aceptarán fotografías de plantas cultivadas, de maceta o jardín.",
 	                 naturaleza_y_ser_humano: "Refiere a usos y costumbres relacionadas con el entorno natural (actividades agrícolas, artesanales, gastronómicas, medicinales, espirituales, recreativas, ceremoniales), manejo sustentable, conservación y/o restauración, también se considera en esta categoría, actividades que impacten de manera negativa, directa o indirectamente la biodiversidad."},
-	                {paisajes_con_ecosistemas: "Vistas aéreas y terrestres de ecosistemas no perturbados de México, desde las costas hasta las altas montañas.",
+	                {paisajes_con_ecosistemas: "Imágenes de ecosistemas no perturbados de México, desde las costas hasta las altas montañas.",
 	                 fotografía_aérea: "Imágenes de animales, plantas, ecosistemas o paisajes desde el aire.",
 	                 fotografía_subacuática:"Imágenes de animales, plantas o hábitats subacuáticos, ya sea en un entorno marino o dulceacuícola. No se aceptarán fotografías de acuario.",
 	                 cineminuto: "(Video) Breves historias de nuestra naturaleza, su uso, conservación o deterioro."},
