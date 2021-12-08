9cd54413eca1613866c47fc973180363cf228e3e
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2021-12-08 07:52:41 -0600

    ahora solo se permite ingresar la cantidad correcta de media, ni una mas ni una menos :D

--- app/assets/javascripts/mosaico_natura/mosaico_natura_extra.js
+++ app/assets/javascripts/mosaico_natura/mosaico_natura_extra.js
@@ -11,8 +11,33 @@ $(document).ready(function(){
 		});
 		$('[data-toggle="tooltip"]').tooltip();
 		$('.toast').toast('show');
-		$('#inscripcion').on('click', '.links a', function(){
-				$(this).hide();
+		
+		//Coloca los nested-fields despues de los botones (.links)
+		$("#medias a.add_fields").data("association-insertion-method", 'after');
+		
+
+		// limits the number of categories
+		check_to_hide_or_show_add_link();
+		
+		$('#medias').on('cocoon:after-insert', function() {
+				check_to_hide_or_show_add_link();
 		});
+		
+		$('#medias').on('cocoon:after-remove', function() {
+				check_to_hide_or_show_add_link();
+		});
+		
+		function check_to_hide_or_show_add_link() {
+				$("#medias a.add_fields").each(function () {
+						link = $(this);
+						nestedClass = $('.nested-fields-for-'+link.data('categoria')+':visible');
+						link.attr('data-after',link.data('maximos') - nestedClass.length);
+						if (nestedClass.length == link.data('maximos')) {
+								link.removeClass('d-inline-flex').addClass('d-none');
+						} else {
+								link.addClass('d-inline-flex').removeClass('d-none');
+						}
+				});
+		}
 });
 
