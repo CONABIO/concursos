db89a18a794a62b28bc6cb451663c1a82993a02b
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2021-12-10 05:45:12 -0600

    la validacion del tamaño se hace enteramente en el browser

--- app/assets/javascripts/mosaico_natura/mosaico_natura_extra.js
+++ app/assets/javascripts/mosaico_natura/mosaico_natura_extra.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 		//Coloca los nested-fields despues de los botones (.links)
 		$("#medias a.add_fields").data("association-insertion-method", 'after');
 		
-
+		
 		// limits the number of categories
 		check_to_hide_or_show_add_link();
 		
@@ -39,5 +39,42 @@ $(document).ready(function(){
 						}
 				});
 		}
+		
+		$("#medias").on('change', '.file-to-upload', function(e) {
+				
+				if(this.files[0].type.indexOf('video') == 0){
+						return true;
+				}
+				
+				var reader = new FileReader();
+				//Read the contents of Image File.
+				reader.readAsDataURL(this.files[0]);
+				reader.onload = function (e) {
+						
+						//Initiate the JavaScript Image object.
+						var image = new Image();
+						
+						//Set the Base64 string return from FileReader as source.
+						image.src = e.target.result;
+						
+						//Validate the File Height and Width.
+						image.onload = function () {
+								var h = this.height;
+								var w = this.width;
+								console.log(h + " - " + w);
+								var largerSide = h > w ? h : w;
+								console.log(largerSide);
+								if (largerSide > 3500 && largerSide < 4500) {
+										console.log("Imagen con medidas adecuadas");
+										return true;
+								}else {
+										alert("Cambia tu foto, no cumple con las medidas mínimas/máximas especificadas en las bases del concurso:\n(Tamaño mínimo: 3,500 pixeles por lado más grande\nTamaño máximo: 4,500 pixeles por lado más grande)\nde subirla sera descalificada.");
+										return false;
+								}
+								
+						};
+				};
+		});
+		
 });
 
