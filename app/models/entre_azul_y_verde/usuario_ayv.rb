e4a450ad693fb47427c4bbd789596a006dfb9dbe
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-03-10 01:20:03 -0600

    codigo mas limpio

--- app/models/entre_azul_y_verde/usuario_ayv.rb
+++ app/models/entre_azul_y_verde/usuario_ayv.rb
@@ -24,6 +24,18 @@ class UsuarioAyv < Usuario
 	
 	scope :dibujos, -> { select(:id, :nombre, :apellido_paterno, :apellido_materno, :fecha_nacimiento, "medias.id as id1, medias.original_filename as proceso, media_bis_usuarios.id as id2, media_bis_usuarios.original_filename as terminado").left_joins(:media).where('medias.posicion = 1').left_joins(:media_bis).where('media_bis_usuarios.posicion = 2').where_basico }
 	
+	scope :menores_a_6, -> { dibujos.where("usuarios.fecha_nacimiento > \"#{Date.new(2016,2,28)}\"") }
+	
+	scope :de_6_a_8, -> { dibujos.where("usuarios.fecha_nacimiento < \"#{Date.new(2016,3,1)}\" and usuarios.fecha_nacimiento > \"#{Date.new(2013,3,1)}\"") }
+	
+	scope :de_9_a_11, -> { dibujos.where("usuarios.fecha_nacimiento < \"#{Date.new(2013,2,28)}\" and usuarios.fecha_nacimiento > \"#{Date.new(2010,3,1)}\"") }
+	
+	scope :de_12_a_14, -> { dibujos.where("usuarios.fecha_nacimiento < \"#{Date.new(2010,2,28)}\" and usuarios.fecha_nacimiento > \"#{Date.new(2007,3,1)}\"") }
+	
+	scope :de_15_a_17, -> { dibujos.where("usuarios.fecha_nacimiento < \"#{Date.new(2007,2,28)}\" and usuarios.fecha_nacimiento > \"#{Date.new(2004,2,28)}\"") }
+	
+	scope :mayores_a_17, -> { dibujos.where("usuarios.fecha_nacimiento < \"#{Date.new(2004,3,1)}\"") }
+	
 	def age_in_completed_years (bd)
 		# Difference in years, less one if you have not had a birthday this year.
 		a = FECHA_TERMINO.year - bd.year
