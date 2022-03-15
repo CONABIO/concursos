016682fa75259eb4d7b65e06e7cc6050b76a14a0
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-03-15 02:47:07 -0600

    metiendo tabla para calificaciones

--- app/models/entre_azul_y_verde/media_ayv.rb
+++ app/models/entre_azul_y_verde/media_ayv.rb
@@ -4,6 +4,9 @@ class MediaAyv < Media
 	belongs_to :usuario, class_name: "UsuarioAyv"
 	has_one :media_metadato, inverse_of: :media, foreign_key: :media_id, class_name: "MediaMetadatoAyv", dependent: :destroy
 	accepts_nested_attributes_for :media_metadato, allow_destroy: true
+	
+	has_one :calificacion, inverse_of: :media, foreign_key: :media_id, class_name: "CalificacionAyv"
+	accepts_nested_attributes_for :calificacion, allow_destroy: true
 
 	validates_presence_of :original_filename
 
