a5d64cf8057e3c72b82768a353c501c046111d7d
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-04-26 16:16:32 -0500

    ya?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,23 @@ class ApplicationController < ActionController::Base
 
   # Limita la aplicacion a un usuario y contrasenia general
   def authenticate
+	  @juez = nil
 	  authenticate_or_request_with_http_basic do |username, password|
-		  username == Rails.application.secrets.usuario_calificacion && password == Rails.application.secrets.password_calificacion
+		  case username
+			  when Rails.application.secrets.dgcc
+				  password == Rails.application.secrets.password_dgcc
+			  when Rails.application.secrets.juez0
+				  @juez = 0
+				  password == Rails.application.secrets.password_juez0
+			  when Rails.application.secrets.juez1
+				  @juez = 1
+				  password == Rails.application.secrets.password_juez1
+			  when Rails.application.secrets.juez2
+				  @juez = 2
+				  password == Rails.application.secrets.password_juez2
+			  else
+				  false
+		  end
 	  end
   end
 
