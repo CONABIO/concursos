21c1305b8825e8d05a6bc78c72e30132851a373c
Alberto González <albertoglezba@gmail.com>
2022-05-01 17:53:12 -0500

    no me gusta, usemos cards!!

--- app/controllers/entre_azul_y_verde/galerias_controller.rb
+++ app/controllers/entre_azul_y_verde/galerias_controller.rb
@@ -2,9 +2,9 @@ class EntreAzulYVerde::GaleriasController < EntreAzulYVerde::EntreAzulYVerdeCont
 
   def index
     @dibujos = {}
-    @dibujos[:menores_a_9] = UsuarioAyv.dibujos_ganadores.menores_a_9
+    @dibujos[:de_6_a_8] = UsuarioAyv.dibujos_ganadores.menores_a_9
     @dibujos[:de_9_a_11] = UsuarioAyv.dibujos_ganadores.de_9_a_11
     @dibujos[:de_12_a_14] = UsuarioAyv.dibujos_ganadores.de_12_a_14
-    @dibujos[:mayores_a_14] = UsuarioAyv.dibujos_ganadores.mayores_a_14
+    @dibujos[:de_15_a_17] = UsuarioAyv.dibujos_ganadores.mayores_a_14
   end
 end
