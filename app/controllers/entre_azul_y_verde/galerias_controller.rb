258ebc309bd8ec98e887586dbc188d9565ed2457
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-13 15:10:10 -0500

    añadiendo menciones

--- app/controllers/entre_azul_y_verde/galerias_controller.rb
+++ app/controllers/entre_azul_y_verde/galerias_controller.rb
@@ -1,10 +1,18 @@
 class EntreAzulYVerde::GaleriasController < EntreAzulYVerde::EntreAzulYVerdeController
 
   def index
-    @dibujos = {}
-    @dibujos[:de_6_a_8] = UsuarioAyv.dibujos_ganadores.menores_a_9
-    @dibujos[:de_9_a_11] = UsuarioAyv.dibujos_ganadores.de_9_a_11
-    @dibujos[:de_12_a_14] = UsuarioAyv.dibujos_ganadores.de_12_a_14
-    @dibujos[:de_15_a_17] = UsuarioAyv.dibujos_ganadores.mayores_a_14
+    @ganadores = {}
+    @ganadores[:de_6_a_8] = UsuarioAyv.dibujos_ganadores.menores_a_9
+    @ganadores[:de_9_a_11] = UsuarioAyv.dibujos_ganadores.de_9_a_11
+    @ganadores[:de_12_a_14] = UsuarioAyv.dibujos_ganadores.de_12_a_14
+    @ganadores[:de_15_a_17] = UsuarioAyv.dibujos_ganadores.mayores_a_14
+	  
+    @menciones = {}
+    Direccion::ESTADOS.each do |x|
+	    mencion = UsuarioAyv.dibujos_para_menciones(x[1])
+	    next unless mencion.present?
+	    @menciones[x[0]] = mencion
+    end
+	  
   end
 end
