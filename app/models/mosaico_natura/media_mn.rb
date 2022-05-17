4ea6c28df26038bf3762d73ce917000b58a6a9ae
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-17 02:35:11 -0500

    faltan joins

--- app/models/mosaico_natura/media_mn.rb
+++ app/models/mosaico_natura/media_mn.rb
@@ -5,10 +5,19 @@ class MediaMn < Media
 	has_one :media_metadato, inverse_of: :media, foreign_key: :media_id, class_name: "MediaMetadatoMn", dependent: :destroy
 	accepts_nested_attributes_for :media_metadato, allow_destroy: true
 
+	has_one :calificacion, inverse_of: :media, foreign_key: :media_id, class_name: "CalificacionMn"
+	accepts_nested_attributes_for :calificacion, allow_destroy: true
+	
 	validates_presence_of :original_filename, :categoria_id
 
 	mount_uploader :original_filename, MediaAwsUploader
 	
 	scope :mosaico, -> { where(categoria: (1..9)) }
 
+	
+	
+	
+	def genera_filename_anonimo
+		[self.usuario_id, self.categoria_id, self.id, self.created_at.strftime('%Y%m%d%H%M%S'),dame_extension(self.original_filename_identifier)].join('_')
+	end
 end
