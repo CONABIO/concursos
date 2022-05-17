6b09a91caebae31a9fb3852aa2792f8edf1122cc
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-17 13:54:33 -0500

    qeudo la edad lista, TERMINEEEEEEEEEEEEEEEEEEEE!!!!!! YEEEEEI

--- app/models/mosaico_natura/media_mn.rb
+++ app/models/mosaico_natura/media_mn.rb
@@ -1,5 +1,7 @@
 class MediaMn < Media
 	
+	FECHA_TERMINO_INICIAL = Date.new(2022, 01, 16)
+	
 	belongs_to :usuario, class_name: "UsuarioMn"
 	belongs_to :categoria, class_name: "CategoriaMn"
 	has_one :media_metadato, inverse_of: :media, foreign_key: :media_id, class_name: "MediaMetadatoMn", dependent: :destroy
@@ -14,7 +16,7 @@ class MediaMn < Media
 	scope :mosaico, -> { where(categoria: (1..9))}
 	scope :where_fotos, -> { where('posicion IS NULL').where('usuarios.concurso_id' => 2) }
 
-	scope :select_finalistas, -> { select(:id, :original_filename, 'media_metadatos.titulo', :descripcion, :marca, :localidad, :otra_marca, :calificacion, :usuario_id, :categoria_id, :created_at, :nombre_categoria) }
+	scope :select_finalistas, -> { select(:id, :original_filename, 'media_metadatos.titulo', :descripcion, :marca, :localidad, :otra_marca, :calificacion, :usuario_id, :fecha_nacimiento, :categoria_id, :created_at, :nombre_categoria) }
 	
 	scope :joins_finalistas, -> { left_joins(:media_metadato, :categoria, :usuario).joins(:calificaciones) }
 	
@@ -24,4 +26,19 @@ class MediaMn < Media
 	def genera_filename_anonimo
 		[self.usuario_id, self.categoria_id, self.id, self.created_at.strftime('%Y%m%d%H%M%S'),dame_extension(self.original_filename_identifier)].join('_')
 	end
+	
+	def age_in_completed_years(bd)
+		# Difference in years, less one if you have not had a birthday this year.
+		a = FECHA_TERMINO_INICIAL.year - bd.year
+		a = a - 1 if (
+		bd.month > FECHA_TERMINO_INICIAL.month or
+				(bd.month >= FECHA_TERMINO_INICIAL.month and bd.day > FECHA_TERMINO_INICIAL.day)
+		)
+		a
+	end
+	
+	def edad
+		age_in_completed_years(self.fecha_nacimiento)
+	end
+	
 end
