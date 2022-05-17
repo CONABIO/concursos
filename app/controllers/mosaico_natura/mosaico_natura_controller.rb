eb3cf837049d1a5ffe1c2be770678cd157fe014d
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-16 23:11:53 -0500

    initial files for calificacion de jueces de mn

--- app/controllers/mosaico_natura/mosaico_natura_controller.rb
+++ app/controllers/mosaico_natura/mosaico_natura_controller.rb
@@ -1,3 +1,34 @@
 class MosaicoNatura::MosaicoNaturaController < ApplicationController
-    layout 'mosaico_natura'
+	layout 'mosaico_natura'
+	
+	protected
+	
+	# Limita la aplicacion a un usuario y contrasenia general
+	def authenticate
+		@juez = nil
+		authenticate_or_request_with_http_basic do |username, password|
+			case username
+				when Rails.application.secrets.mn
+					@juez = 3
+					password == Rails.application.secrets.password_dgcc
+				when Rails.application.secrets.juez0
+					@juez = 0
+					password == Rails.application.secrets.password_juez_foto_0
+				when Rails.application.secrets.juez1
+					@juez = 1
+					password == Rails.application.secrets.password_juez_foto_1
+				when Rails.application.secrets.juez2
+					@juez = 2
+					password == Rails.application.secrets.password_juez_foto_2
+				when Rails.application.secrets.juez2
+					@juez = 2
+					password == Rails.application.secrets.password_juez_video_0
+				when Rails.application.secrets.juez2
+					@juez = 2
+					password == Rails.application.secrets.password_juez_video_1
+				else
+					false
+			end
+		end
+	end
 end
