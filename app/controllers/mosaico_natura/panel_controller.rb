1c095da9cdd90e34b9505902b9bbda4f35071c71
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-17 02:35:33 -0500

    renombrando variables

--- app/controllers/mosaico_natura/panel_controller.rb
+++ app/controllers/mosaico_natura/panel_controller.rb
@@ -2,6 +2,10 @@ class MosaicoNatura::PanelController < MosaicoNatura::MosaicoNaturaController
 	
 	def calificacion
 		@fotos = {}
+		CategoriaMn.all.each do |c|
+			@fotos[c.nombre_categoria] = MediaMn.mosaico.where(categoria_id: c.id)
+		end
+	
 	end
 	
 	def desempate
