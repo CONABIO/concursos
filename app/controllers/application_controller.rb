136eac36e4f46a483e200914e5fa32300536eb16
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-17 10:47:23 -0500

    cambio de lineas para q ayv no pueda calificar en mn y viceversa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,19 +6,31 @@ class ApplicationController < ActionController::Base
 	# Limita la aplicacion a un usuario y contrasenia general
 	def authenticate
 		@juez = nil
+		authenticate_ayv || authenticate_mn
+	end
+	
+	def authenticate_ayv
 		authorized = false
 		authenticate_or_request_with_http_basic do |username, password|
 			Rails.application.secrets.entre_azul_y_verde.each do |k,v|
 				authorized = (v[:usuario] == username && v[:password] == password)
-				@juez = v[:posicion] if authorized
+				@juez = v if authorized
 				return if authorized
 			end
+			authorized
+		end
+	end
+	
+	def authenticate_mn
+		authorized = false
+		authenticate_or_request_with_http_basic do |username, password|
 			Rails.application.secrets.mosaico_natura.each do |k,v|
 				authorized = (v[:usuario] == username && v[:password] == password)
-				@juez = v[:posicion] if authorized
+				@juez = v if authorized
 				return if authorized
 			end
 			authorized
 		end
 	end
 end
+
