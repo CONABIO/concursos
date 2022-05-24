f7b84f378af004e49d1f8edfdb3cfbb4eb906026
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-24 14:23:52 -0500

    frontend para desempatar

--- app/models/mosaico_natura/media_mn.rb
+++ app/models/mosaico_natura/media_mn.rb
@@ -3,6 +3,8 @@ class MosaicoNatura::MediaMn < Media
 	FECHA_TERMINO_INICIAL = Date.new(2022, 01, 16)
 	
 	belongs_to :usuario, class_name: "UsuarioMn"
+	has_one :direccion, through: :usuario, source: :direccion
+
 	belongs_to :categoria, class_name: "CategoriaMn"
 	has_one :media_metadato, inverse_of: :media, foreign_key: :media_id, class_name: "MediaMetadatoMn", dependent: :destroy
 	accepts_nested_attributes_for :media_metadato, allow_destroy: true
@@ -15,13 +17,18 @@ class MosaicoNatura::MediaMn < Media
 	
 	scope :mosaico, -> { where(categoria: (1..9))}
 	scope :where_fotos, -> { where('posicion IS NULL').where('usuarios.concurso_id' => 2) }
-
-	scope :select_finalistas, -> { select(:id, :original_filename, 'media_metadatos.titulo', :descripcion, :marca, :localidad, :otra_marca, :calificacion, :usuario_id, :fecha_nacimiento, :categoria_id, :created_at, :nombre_categoria) }
 	
-	scope :joins_finalistas, -> { left_joins(:media_metadato, :categoria, :usuario).joins(:calificaciones) }
 	
-	scope :finalistas, -> { select_finalistas.joins_finalistas.where_fotos }
+	scope :select_medias, -> { select(:id, :original_filename, 'media_metadatos.titulo', :descripcion, :marca, :localidad, :otra_marca, :calificacion, :usuario_id, :fecha_nacimiento, :categoria_id, :created_at, :nombre_categoria) }
+	scope :select_promedio_fotos, -> { select("(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),3,1) + substr(cast(calificacion as char),4,1))/4 as promedio") }
+	scope :select_promedio_videos, -> { select("(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1))/2 as promedio") }
 	
+	scope :joins_con_calificacion, -> { left_joins(:media_metadato, :categoria, :usuario).joins(:calificaciones) }
+	scope :joins_con_calificacion_direccion, -> { left_joins(:media_metadato, :categoria).joins(:calificaciones, :direccion) }
+	
+	scope :finalistas, -> { select_medias.joins_con_calificacion.where_fotos }
+	scope :desempate_foto, -> { select_medias.select_promedio_fotos.joins_con_calificacion_direccion.where('categoria_id != 8').order('promedio DESC').limit(10) }
+	scope :desempate_video, -> { select_medias.select_promedio_videos.joins_con_calificacion_direccion.where('categoria_id' => 8).order('promedio DESC').limit(6) }
 	
 	def genera_filename_anonimo
 		[self.usuario_id, self.categoria_id, self.id, self.created_at.strftime('%Y%m%d%H%M%S'),dame_extension(self.original_filename_identifier)].join('_')
