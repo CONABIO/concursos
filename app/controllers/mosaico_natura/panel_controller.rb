fe5f4b85feb4411ae4d1672e7cb7778300776b64
Gerardo Alberto González Bautista <albertoglezba@gmail.com>
2022-05-26 09:40:34 -0500

    cambios para poder generar el json para los listados

--- app/controllers/mosaico_natura/panel_controller.rb
+++ app/controllers/mosaico_natura/panel_controller.rb
@@ -23,6 +23,32 @@ class MosaicoNatura::PanelController < MosaicoNatura::MosaicoNaturaController
 				end
 			end
 		end
+		
+		respond_to do |format|
+			format.html
+			format.json { render json: @medias.to_json }
+		end
+	end
+	
+	def ganadores
+		@medias = {}
+		if params[:categoria].present?
+			categoria = params[:categoria]
+			@medias[categoria] = (categoria == 'cineminuto') ?  MosaicoNatura::MediaMn.desempate_video :  MosaicoNatura::MediaMn.desempate_foto.where("nombre_categoria = '#{categoria}'")
+		else
+			MosaicoNatura::CategoriaMn.all.each do |c|
+				unless c.id==8
+					@medias[c.nombre_categoria] = MosaicoNatura::MediaMn.ganadores_foto.where(categoria_id: c.id)
+				else
+					@medias[c.nombre_categoria] = MosaicoNatura::MediaMn.ganadores_video
+				end
+			end
+		end
+		
+		respond_to do |format|
+			format.html
+			format.json { render json: @medias.to_json, status: :ok}
+		end
 	end
 	
 end
--- app/models/mosaico_natura/media_mn.rb
+++ app/models/mosaico_natura/media_mn.rb
@@ -1,5 +1,6 @@
 class MosaicoNatura::MediaMn < Media
 	
+	#attr_accessor :filename
 	FECHA_TERMINO_INICIAL = Date.new(2022, 01, 16)
 	
 	belongs_to :usuario, class_name: "UsuarioMn"
@@ -19,21 +20,31 @@ class MosaicoNatura::MediaMn < Media
 	scope :where_fotos, -> { where('posicion IS NULL').where('usuarios.concurso_id' => 2) }
 	
 	
-	scope :select_medias, -> { select(:id, :original_filename, 'media_metadatos.titulo', :descripcion, :marca, :localidad, :otra_marca, :calificacion, :usuario_id, :fecha_nacimiento, :categoria_id, :created_at, :nombre_categoria) }
+	scope :select_medias, -> { select(:id, "original_filename as archivo_original",:filename,  'media_metadatos.titulo', :descripcion, :marca, :localidad, :otra_marca, :calificacion, :usuario_id, :fecha_nacimiento, :categoria_id, :created_at, :nombre_categoria) }
 	scope :select_promedio_fotos, -> { select("(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),3,1) + substr(cast(calificacion as char),4,1))/4 as promedio") }
-	scope :select_promedio_comparativo, -> { select("(substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),3,1) + substr(cast(calificacion as char),4,1))/3 as promedio_sin_juez01", "(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),3,1) + substr(cast(calificacion as char),4,1))/3 as promedio_sin_juez02", "(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),4,1))/3 as promedio_sin_juez03", "(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),3,1))/3 as promedio_sin_juez04") }
 	scope :select_promedio_videos, -> { select("(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1))/2 as promedio") }
-	scope :select_promedio_comparativo_videos, -> { select("substr(cast(calificacion as char),2,1) as promedio_sin_juez01", "substr(cast(calificacion as char),1,1) as promedio_sin_juez02", "'' as promedio_sin_juez03", "'' as promedio_sin_juez04")  }
+	scope :select_promedio_comparativo_fotos, -> { select_promedio_fotos.select("(substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),3,1) + substr(cast(calificacion as char),4,1))/3 as promedio_sin_juez01", "(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),3,1) + substr(cast(calificacion as char),4,1))/3 as promedio_sin_juez02", "(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),4,1))/3 as promedio_sin_juez03", "(substr(cast(calificacion as char),1,1) + substr(cast(calificacion as char),2,1) + substr(cast(calificacion as char),3,1))/3 as promedio_sin_juez04") }
+	scope :select_promedio_comparativo_videos, -> { select_promedio_videos.select("substr(cast(calificacion as char),2,1) as promedio_sin_juez01", "substr(cast(calificacion as char),1,1) as promedio_sin_juez02", "'' as promedio_sin_juez03", "'' as promedio_sin_juez04")  }
+	scope :select_datos_usuario, -> { select("nombre", "apellido_paterno", "apellido_materno")}
+	scope :select_ganadores, -> { select_medias}
 	
 	scope :joins_con_calificacion, -> { left_joins(:media_metadato, :categoria, :usuario).joins(:calificaciones) }
-	scope :joins_con_calificacion_direccion, -> { left_joins(:media_metadato, :categoria).joins(:calificaciones, :direccion) }
+	scope :joins_con_calificacion_direccion, -> { left_joins(:media_metadato, :categoria).joins(:direccion, :calificaciones) }
 	
 	scope :finalistas, -> { select_medias.joins_con_calificacion.where_fotos }
-	scope :desempate_foto, -> { select_medias.select_promedio_fotos.select_promedio_comparativo.joins_con_calificacion_direccion.where('categoria_id != 8').order('promedio DESC') }
-	scope :desempate_video, -> { select_medias.select_promedio_videos.select_promedio_comparativo_videos.joins_con_calificacion_direccion.where('categoria_id' => 8).order('promedio DESC') }
+	scope :desempate_foto, -> { select_medias.select_promedio_comparativo_fotos.joins_con_calificacion_direccion.where('categoria_id != 8').order('promedio DESC') }
+	scope :desempate_video, -> { select_medias.select_promedio_comparativo_videos.joins_con_calificacion_direccion.where('categoria_id' => 8).order('promedio DESC') }
+	
+	scope :ganadores_foto, -> { }
+	scope :ganadores_video, -> { }
+	
+	
+	def filename
+		[self.usuario_id, self.categoria_id, self.id, self.created_at.strftime('%Y%m%d%H%M%S'),dame_extension(self.archivo_original)].join('_')
+	end
 	
 	def genera_filename_anonimo
-		[self.usuario_id, self.categoria_id, self.id, self.created_at.strftime('%Y%m%d%H%M%S'),dame_extension(self.original_filename_identifier)].join('_')
+		filename
 	end
 	
 	def age_in_completed_years(bd)
