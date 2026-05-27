5d9015fb2aaad866083f23502fb1776e57f0f56b
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-05-27 10:48:54 -0600

    reparacion de fechas

--- app/models/mosaico_natura/usuario_mn.rb
+++ app/models/mosaico_natura/usuario_mn.rb
@@ -10,6 +10,9 @@ class MosaicoNatura::UsuarioMn < Usuario
         Date.today - EDAD_MINIMA.years
     end
     
+    # Fecha mínima (dinámica: hoy - 14 años)
+    FECHA_NAC_MIN = fecha_nac_min_adultos.freeze
+    
     # Fecha límite: nacidos en 2012 o antes (14 años cumplidos en 2026)
     # Ejemplo: si hoy es 2026, FECHA_NAC_MAX_LIMITE = 2012-12-31 (los de 2012 aún entran)
     FECHA_NAC_MAX_LIMITE = Date.new(2012, 12, 31).freeze
