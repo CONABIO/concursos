ebd2032d62d88c2e44a3bef725decb4a6b5793f5
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-05-27 10:53:56 -0600

    reparando variables de fechas

--- app/models/mosaico_natura/usuario_mn.rb
+++ app/models/mosaico_natura/usuario_mn.rb
@@ -13,6 +13,9 @@ class MosaicoNatura::UsuarioMn < Usuario
     # Fecha mínima (dinámica: hoy - 14 años)
     FECHA_NAC_MIN = fecha_nac_min_adultos.freeze
     
+    # Constante para el controlador (nombre en mayúsculas que busca)
+    FECHA_NAC_MIN_ADULTOS = fecha_nac_min_adultos.freeze
+    
     # Fecha límite: nacidos en 2012 o antes (14 años cumplidos en 2026)
     # Ejemplo: si hoy es 2026, FECHA_NAC_MAX_LIMITE = 2012-12-31 (los de 2012 aún entran)
     FECHA_NAC_MAX_LIMITE = Date.new(2012, 12, 31).freeze
