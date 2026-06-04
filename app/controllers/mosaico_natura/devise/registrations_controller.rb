3b1b884968318f9333aac05d33c1aba631a8a5ff
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-06-04 14:56:47 -0600

    reparacion de ruta despues de inicio de sesion

--- app/controllers/mosaico_natura/devise/registrations_controller.rb
+++ app/controllers/mosaico_natura/devise/registrations_controller.rb
@@ -1,9 +1,5 @@
 class MosaicoNatura::Devise::RegistrationsController < ::Devise::RegistrationsController
-  layout 'mosaico_natura'
-  
-  def after_sign_up_path_for(resource)
-    new_mosaico_natura_registro_path
-  end  
+  layout 'mosaico_natura' 
 
    def after_sign_up_path_for(resource)
     new_mosaico_natura_registro_path
