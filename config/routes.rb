a0c5710ea09d3c0aa3ba83267005059d96fff6d9
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-07-08 10:11:43 -0600

    rutas para selector

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 	resources :usuarios
 	resources :medias
 	resources :calificaciones
+  resources :selector, only: [:index, :update]
+
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	if Rails.env.development?
 		namespace 'entre_azul_y_verde' do
