e2f8a470a7527b53d5d85623f99f2bfad3f49403
Julio Axel Gonzalez Morales <axell@MacBook-Air-de-Julio.local>
2026-08-14 15:01:40 -0600

    optimizar imagennes

--- app/uploaders/media_aws_uploader.rb
+++ app/uploaders/media_aws_uploader.rb
@@ -1,23 +1,27 @@
 class MediaAwsUploader < CarrierWave::Uploader::Base
-  # Storage configuration within the uploader supercedes the global CarrierWave
-  # config, so either comment out `storage :file`, or remove that line, otherwise
-  # AWS will not be used.
+  include CarrierWave::MiniMagick
+
   storage :aws
 
-  # You can find a full list of custom headers in AWS SDK documentation on
-  # AWS::S3::S3Object
   def download_url(filename)
     url(response_content_disposition: %Q{attachment; filename="#{filename}"})
   end
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     concurso = model.class.to_s.underscore.split("_").last
     "media/#{concurso}/#{model.usuario.id}"
-  end  
+  end
 
   def extension_allowlist
-     %w(jpg jpeg png mp4)
-  end  
+    %w(jpg jpeg png mp4)
+  end
+
+  version :galeria, if: :es_imagen? do
+    process resize_to_limit: [1200, 1200]
+    process convert: "jpg"
+  end
+
+  def es_imagen?(file)
+    %w(image/jpeg image/png).include?(file.content_type)
+  end
 end
